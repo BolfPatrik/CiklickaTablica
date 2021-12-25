8e05f67dd4d98284be56aa82a30d77b1311255cd
BolfPatrik <bolfpatrik0@gmail.com>
2021-12-25 20:53:50 +0100

    Dodan uvjet za korisnika
    Korisnik ne smije unijeti negativan broj ili unijeti nulu na oba dva mjesta.

--- CiklickaTablica.java
+++ CiklickaTablica.java
@@ -4,9 +4,15 @@ import javax.swing.JOptionPane;
 
 public class CiklickaTablica {
 public static void main(String[] args) {
+    while(true) {
 		int x = Integer.parseInt(JOptionPane.showInputDialog("Unesi broj redaka"));
 		int y = Integer.parseInt(JOptionPane.showInputDialog("Unesi broj stupaca"));
 		int tablica[][] = new int[x][y];
+        if(x<1 && y<1) {
+			JOptionPane.showMessageDialog(null, "Unesi broj veći od nule barem na jednom od dva unosa");
+			continue;
+        }
+    }
 		
 }
 }
