1f2eca7eb9d8e42f14d1a0fb8e7f54384b674554
BolfPatrik <bolfpatrik0@gmail.com>
2021-12-25 20:50:49 +0100

    Uneseno za korisnika
    Nisam siguran je li bi trebao napraviti unose da budu u nizu ili na ovaj način.

---
+++ CiklickaTablica.java
@@ -0,0 +1,13 @@
+package edunova;
+
+import javax.swing.JOptionPane;
+
+public class CiklickaTablica {
+public static void main(String[] args) {
+		int x = Integer.parseInt(JOptionPane.showInputDialog("Unesi broj redaka"));
+		int y = Integer.parseInt(JOptionPane.showInputDialog("Unesi broj stupaca"));
+		int tablica[][] = new int[x][y];
+		
+}
+}
+
