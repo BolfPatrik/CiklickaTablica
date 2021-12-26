b4ffa904e975776979080e527fc316083d412bc5
BolfPatrik <bolfpatrik0@gmail.com>
2021-12-27 00:44:45 +0100

    Postavio zadnji element da bude 1

--- CiklickaTablica.java
+++ CiklickaTablica.java
@@ -7,11 +7,12 @@ public static void main(String[] args) {
 		int x = Integer.parseInt(JOptionPane.showInputDialog("Unesi broj redaka"));
 		int y = Integer.parseInt(JOptionPane.showInputDialog("Unesi broj stupaca"));
 		int tablica[][] = new int[x][y];
+		tablica[x-1][y-1]=1;
 		for(int i=0;i<x;i++) {
 			for(int j=0;j<y;j++) {
 				System.out.printf("%3d", tablica[i][j]);
 	        }
 	        System.out.println();
-		}			
-			}
+	      }		
+			}	
 }
