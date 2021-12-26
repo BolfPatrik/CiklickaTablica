e4af7c4266a03d47c2bed30ff88c1a4b1817312d
BolfPatrik <bolfpatrik0@gmail.com>
2021-12-27 00:12:48 +0100

    Maknuti uvjeti za korisnika, dodan input da izgleda poput matrice
    Za sad ću maknuti uvjete za korisnika kasnije ću se s time zabavit, dodan input za matricu (valjda)

--- CiklickaTablica.java
+++ CiklickaTablica.java
@@ -4,16 +4,14 @@ import javax.swing.JOptionPane;
 
 public class CiklickaTablica {
 public static void main(String[] args) {
-    while(true) {
 		int x = Integer.parseInt(JOptionPane.showInputDialog("Unesi broj redaka"));
 		int y = Integer.parseInt(JOptionPane.showInputDialog("Unesi broj stupaca"));
 		int tablica[][] = new int[x][y];
-        if(x<1 && y<1) {
-			JOptionPane.showMessageDialog(null, "Unesi broj veći od nule barem na jednom od dva unosa");
-			continue;
-        }
-    }
-		
+		for(int i=0;i<x;i++) {
+			for(int j=0;j<y;j++) {
+				System.out.printf("%3d", tablica[i][j]);
+	        }
+	        System.out.println();
+		}			
+			}
 }
-}
-
