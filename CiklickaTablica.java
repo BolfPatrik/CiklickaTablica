9f253ec07491e02531b68525fd305c7500de9ea9
BolfPatrik <bolfpatrik0@gmail.com>
2022-01-10 13:01:16 +0100

    Maknuo postavljenu vrijednost 1,promjenio ispis matrice da bude ljepši,deklarirao početak i kraj reda i stupca

--- CiklickaTablica.java
+++ CiklickaTablica.java
@@ -7,10 +7,12 @@ public static void main(String[] args) {
 		int x = Integer.parseInt(JOptionPane.showInputDialog("Unesi broj redaka"));
 		int y = Integer.parseInt(JOptionPane.showInputDialog("Unesi broj stupaca"));
 		int tablica[][] = new int[x][y];
-		tablica[x-1][y-1]=1;
+		int red=0,krajreda=x-1;
+        int stupac=0,krajstupca=y-1;
+        int brojac=1;
 		for(int i=0;i<x;i++) {
 			for(int j=0;j<y;j++) {
-				System.out.printf("%3d", tablica[i][j]);
+				System.out.print(tablica[i][j]+ "\t");
 	        }
 	        System.out.println();
 	      }		
