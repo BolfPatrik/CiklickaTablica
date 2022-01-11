f8e00c7e0fea98ca4543977300675f6269c84e91
BolfPatrik <bolfpatrik0@gmail.com>
2022-01-11 19:55:34 +0100

    Dodani Ifovi

--- CiklickaTablica.java
+++ CiklickaTablica.java
@@ -1,50 +1,52 @@
 package edunova;
-
 import javax.swing.JOptionPane;
-
 public class CiklickaTablica {
-public static void main(String[] args) {
-		int x = Integer.parseInt(JOptionPane.showInputDialog("Unesi broj redaka"));
-		int y = Integer.parseInt(JOptionPane.showInputDialog("Unesi broj stupaca"));
-		int tablica[][] = new int[x][y];
-		int red=0,krajreda=x-1;
+	public static void main(String[] args) {
+        int x = Integer.parseInt(JOptionPane.showInputDialog("Unesi broj redaka"));
+        int y = Integer.parseInt(JOptionPane.showInputDialog("Unesi broj stupaca"));
+        int tablica[][] = new int[x][y];
+        int red=0,krajreda=x-1;
         int stupac=0,krajstupca=y-1;
         int brojac=1;
-		while(red<=krajreda && stupac <=krajstupca) {
+        while(red<=krajreda && stupac <=krajstupca) {
             for(int i=krajstupca;i>=stupac;i--) {
                 tablica[krajreda][i]=brojac;
-                brojac++;  
-				if(brojac>x*y) {
-                	krajstupca-=1;
-                    break;
-                }
-			}
-			krajreda--;
-			 for(int i=krajreda;i>=red;i--) {
-                tablica[i][stupac]=brojac;
-                brojac++;
-				if(brojac>x*y) {
-                	red-=1;
-                    break;
+                brojac++;              
             }
-			 }
-			stupac++;
+            if(brojac>x*y) {        
+                break;
+            }
+            krajreda--;
+            for(int i=krajreda;i>=red;i--) {
+                tablica[i][stupac]=brojac;
+                brojac++; 
+            }
+            if(brojac>x*y) {        
+                break;
+            }
+            stupac++;
             for(int i=stupac;i<=krajstupca;i++) {
                 tablica[red][i]=brojac;
-                brojac++;
-		}
-		red++;
+                brojac++;       
+            }
+            if(brojac>x*y) {
+                break;
+        }
+            red++;
             for(int i=red;i<=krajreda;i++) {
                 tablica[i][krajstupca]=brojac;
-                brojac++;
-			}
-		krajstupca--;
-		}
-		for(int i=0;i<x;i++) {
-			for(int j=0;j<y;j++) {
-				System.out.print(tablica[i][j]+ "\t");
-	        }
-	        System.out.println();
-	      }		
-			}	
+                brojac++;       
+            }
+            if(brojac>x*y) {        
+                break;
+            }
+            krajstupca--;
+        }
+        for(int i=0;i<x;i++) {
+            for(int j=0;j<y;j++) {
+                System.out.print(tablica[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
 }
